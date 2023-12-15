9bd7ebf52d492af9d9caccf9cda8791e91aa46cf
Apurva <61090951+Apurva272@users.noreply.github.com>
2023-12-15 10:59:36 +0530

    Delete JavaExercise1/BitManipulationJava directory

--- JavaExercise1/BitManipulationJava/ClearBit.java
+++
@@ -1,9 +0,0 @@
-public class ClearBit {
-    public static void main(String[] args) {
-        int n = 5;
-        int pos = 2;
-        int bitmask = ~(1 << pos);
-        int res = bitmask & n;
-        System.out.println(res);
-    }
-}
--- JavaExercise1/BitManipulationJava/SetBit.java
+++
@@ -1,9 +0,0 @@
-public class SetBit {
-    public static void main(String[] args) {
-        int n = 5;
-        int pos = 1;
-        int bitmask = 1 << pos;
-        int res = bitmask | n;
-        System.out.println(res);
-    }
-}
