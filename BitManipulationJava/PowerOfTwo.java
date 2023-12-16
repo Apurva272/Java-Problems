46ae0c0bad283a9e8ca8786dc4e26b0ddcb80e06
Apurva <61090951+Apurva272@users.noreply.github.com>
2023-12-17 00:04:24 +0530

    Update PowerOfTwo.java

--- BitManipulationJava/PowerOfTwo.java
+++ BitManipulationJava/PowerOfTwo.java
@@ -1,15 +1,20 @@
-public class PowerOfTwo {
-    public static void main(String[] args) {
-        int n = 12;
+public static void main(String[] args) {
+        int n = 8;
+        int count = 0;
         while (n != 1) {
-            if (n % 2 == 0) {
-                n = n / 2;
-                System.out.println("Power of 2");
-            } else {
-                System.out.println("Not a power of 2");
+
+            int div = n % 2;
+            if (div != 0) {
+                count++;
             }
+            n = n / 2;
+
+        }
+        if (count == 0) {
+            System.out.print("Power of 2");
+        } else {
+            System.out.print("Not a power of 2");
 
         }
     }
-
 }
