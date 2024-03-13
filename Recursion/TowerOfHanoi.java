377200446f507511d5e4596db77a42702bc0a55e
Apurva <61090951+Apurva272@users.noreply.github.com>
2024-03-13 11:24:43 +0530

    Add files via upload
    Tower of Hanoi, passes Source, destination and helper towers as strings and prints simple statements explaining transfer of disks among the three towers

---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,16 @@
+public class TowerOfHanoi {
+    public static void Tower(int n, String source, String destination, String helper) {
+        if (n == 1) {
+            System.out.println("Transfer of " + n + " from " + source + " to " + destination);
+            return;
+        }
+        Tower(n - 1, source, helper, destination);
+        System.out.println("Transfer of " + n + " from " + source + " to " + destination);
+        Tower(n - 1, helper, destination, source);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        Tower(n, "S", "D", "H");
+    }
+}
