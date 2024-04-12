f7f4942c0302babb8d6a44812b20ec55087fba21
Apurva <61090951+Apurva272@users.noreply.github.com>
2024-04-12 10:00:31 +0530

    Update TowerOfHanoi.java

--- Recursion/TowerOfHanoi.java
+++ Recursion/TowerOfHanoi.java
@@ -1,4 +1,6 @@
 //Time complexity is O(2^n-1)
+// Divide n rings into two parts ring nth = 1 ring, ring n-1 to 1 = 2nd ring.
+    // Now think of them as 2 rings and solve
 public class TowerOfHanoi {
     public static void Tower(int n, String source, String destination, String helper) {
         if (n == 1) {
